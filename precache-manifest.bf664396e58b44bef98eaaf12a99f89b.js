86b2273d4d03baede01c8be38580e1fcaddbabe8
Hyejin00 <svivs137@naver.com>
2020-04-16 16:56:20 +0900

    Updates

---
+++ precache-manifest.bf664396e58b44bef98eaaf12a99f89b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d4e55f0173e3e3d3ae1cce422068f7",
+    "url": "/basic_react_movie/index.html"
+  },
+  {
+    "revision": "4c3a0a179621cdf79b85",
+    "url": "/basic_react_movie/static/css/main.6aa1e2d4.chunk.css"
+  },
+  {
+    "revision": "985286fcbba743487100",
+    "url": "/basic_react_movie/static/js/2.2cfdc291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic_react_movie/static/js/2.2cfdc291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3a0a179621cdf79b85",
+    "url": "/basic_react_movie/static/js/main.d1561125.chunk.js"
+  },
+  {
+    "revision": "32c49bbeebeb9b76a4d4",
+    "url": "/basic_react_movie/static/js/runtime-main.69bcaa4d.js"
+  }
+]);
