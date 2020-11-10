25606dc96a50f530d7c51d6119eb9ce529c91ef5
Hyejin00 <svivs137@naver.com>
2020-11-11 07:10:19 +0900

    Modify Navigation, Detail css and Detail js(add genres, poster, summary, year)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navigation.css';
 
 function Navigation(){
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+    <div className='navigation'>
+      <Link className="navigation__home" to="/">Home</Link>
+      <Link className="navigation__about" to="/about">About</Link>
     </div>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './Detail.css';
 
 class Detail extends React.Component{
   componentDidMount(){
     console.log(this.props);
-    
     const {location, history} = this.props;
     if (location.state === undefined){
       history.goBack();
@@ -14,8 +14,20 @@ class Detail extends React.Component{
   render(){
     const {location} = this.props;
     if (location.state){
+      const {genres, poster, summary, title, year} = location.state;
       return (
-        <span>{location.state.title}</span>
+        <div className="detail">
+          <div className="movie_card">
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie__info">
+              <h2 className="movie__title">{title}</h2>
+              {genres.map((genre,idx) =>
+              <span key={idx} className='genres__genre'>{genre}</span>)}
+              <span className='movie__year'>{year}</span>
+            </div>
+          </div>
+          <span>{summary}</span>
+        </div>
         );
     }else{
       return null;
